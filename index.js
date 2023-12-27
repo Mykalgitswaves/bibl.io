cd9716e7f099be9680d4357d30e8ade46381fa85
Mykalgitswaves <michaelfinal.png@gmail.com>
2023-12-26 22:45:07 -0500

    More styles and nerdage for site.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
+const path = require('path');
 
 const app = express();
 const port = 3000;
@@ -8,6 +9,8 @@ nunjucks.configure('views', {
     autoescape: true,
     express: app
 });
+
+app.use(express.static(path.join(__dirname, '/views/styles')));
 
 app.get('/', (req, res) => {
     res.render('home.njk')
