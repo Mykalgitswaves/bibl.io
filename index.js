ff5da57f253d5f229879572e20a22bfa3260feb0
Mykalgitswaves <michaelfinal.png@gmail.com>
2023-12-27 00:37:38 -0500

    HTMMXXXXXXXX

--- index.js
+++ index.js
@@ -12,8 +12,35 @@ nunjucks.configure('views', {
 
 app.use(express.static(path.join(__dirname, '/views/styles')));
 
-app.get('/', (req, res) => {
-    res.render('home.njk')
+app.use(express.static(path.join(__dirname, '/views/js')));
+
+app.get('/home', (req, res) => {
+    res.render('home.njk');
+});
+
+const book_data = {
+        "book": 'The catcher in the Rye',
+        "description": 'Another banned book',
+        "book-cover": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1446876799l/6759.jpg" 
+    }
+
+app.post('/hero-clicked', (req, res) => {
+    // Is there a way to get our api
+    // recommending books to people 
+    // from a couple of recs?
+
+    const data = `
+        <div class="book-card">
+            <img 
+                src="${book_data['book-cover']}"
+                alt="${book_data['book']}"
+            >
+            <h3>${book_data['book']}</h3>
+            <p>${book_data['description']}</p>
+        </div>
+    `;
+
+    res.send(data);
 });
 
 app.listen(port, () => {
